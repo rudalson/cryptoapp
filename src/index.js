9cd75b3d66e6f27bb9f37a9ef13d9c410f22ccba
rudalson <rudalson@gmail.com>
2022-06-22 17:00:05 +0900

    added : fetch get api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Amplify from 'aws-amplify';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import config from './aws-exports';
+
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
